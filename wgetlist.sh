45b259b33bed7e76e86d2b572f530e4bc4a3c8c6
macubergeek <macubergeek@comcast.net>
2014-09-28 18:00:18 -0400

    adding a few tools

--- wgetlist.sh
+++ wgetlist.sh
@@ -58,6 +58,8 @@ https://github.com/Jermolene/TiddlyWiki5.git
 http://www.gat3way.eu/poc/swscan.tgz
 http://live.sysinternals.com/psexec.exe
 http://www.wxtoimg.com/downloads/wxtoimg-linux64-2.10.11-1.tar.gz
+wget http://debianuser.org/bruteforce/passlist.txt
+wget http://debianuser.org/bruteforce/brutessh.zip
 EOF`
 cd /opt
 for i in $a

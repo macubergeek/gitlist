662d126636021a3a0690003c28b5c158d69047f1
macubergeek <macubergeek@comcast.net>
2014-09-29 05:45:24 -0400

    adding a few tools

--- svn.sh
+++ svn.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 a=`cat <<-EOF
-http://impacket.googlecode.com/svn/trunk/impacket
-http://edgessh.googlecode.com/svn/trunk/ edgessh-read-only
-http://host-extract.googlecode.com/svn/trunk/ host-extract-read-only
-http://subterfuge.googlecode.com/svn/trunk/ subterfuge-read-only
-http://hedyattack.googlecode.com/svn/trunk/ hedyattack-read-only
-svn://hackarmoury.com/live ha
-http://wifite.googlecode.com/svn/trunk/ wifite-read-only
+http://zigbee-security.googlecode.com/svn/trunk/ zigbee-security-read-only`
 EOF`
 cd /opt
 for i in $a
 do
-svn co $i
+svn checkout $i
 done

ab85fb16df8780222aba63d83d0792231c12a8f3
macubergeek <macubergeek@comcast.net>
2014-09-30 06:00:02 -0400

    adding a few tools

--- svn.sh
+++ svn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 a=`cat <<-EOF
 http://zigbee-security.googlecode.com/svn/trunk/ zigbee-security-read-only`
+http://svn.aircrack-ng.org/branch/airoscript-ng/ airoscript-ng
 EOF`
 cd /opt
 for i in $a

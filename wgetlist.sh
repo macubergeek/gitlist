9bb6712c935e7386e09fc81cff6f8eb6d21e703f
macubergeek <macubergeek@comcast.net>
2015-01-19 18:00:01 -0500

    adding a few tools

--- wgetlist.sh
+++ wgetlist.sh
@@ -72,6 +72,7 @@ https://www.trustedsec.com/files/SqlBrute.zip
 https://www.trustedsec.com/files/delldrac.py
 http://download.sysinternals.com/files/PSTools.zip
 https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.2.deb
+http://www.w1hkj.com/downloads/fldigi/fldigi-3.22.05.tar.gz
 EOF`
 cd /opt
 for i in $a

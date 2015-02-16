4cf0d29cffcc56b84560e52d8f2c5d2baba80cff
macubergeek <macubergeek@comcast.net>
2015-02-16 15:00:01 -0500

    adding a few tools

--- gitlist.sh
+++ gitlist.sh
@@ -205,6 +205,8 @@ https://github.com/superkojiman/rfishell.git
 https://github.com/ikkisoft/bradamsa.git
 https://github.com/smicallef/spiderfoot.git
 https://github.com/invisiblethreat/peeper.git
+https://github.com/devttys0/binwalk.git
+https://github.com/devttys0/binwalk.wiki.git
 EOF`
 cd /opt
 for i in $a

1686282a8f3f061bf5fe56c6bd785f29054eef5c
macubergeek <macubergeek@comcast.net>
2015-03-17 18:27:45 -0400

    adding a few tools

--- svn.sh
+++ svn.sh
@@ -4,6 +4,7 @@ http://zigbee-security.googlecode.com/svn/trunk/ zigbee-security-read-only
 http://svn.aircrack-ng.org/branch/airoscript-ng/ airoscript-ng
 http://va-pt.googlecode.com/svn/trunk/ va-pt-read-only
 https://svn.code.sf.net/p/jbrofuzz/code/ jbrofuzz
+http://domsnitch.googlecode.com/svn/trunk/ domsnitch-read-only
 EOF`
 cd /opt
 for i in $a

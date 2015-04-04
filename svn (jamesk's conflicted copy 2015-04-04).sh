e7cf6cbafd82997ec44d200063911311caf47d88
macubergeek <macubergeek@comcast.net>
2015-04-04 12:00:00 -0400

    adding a few tools

---
+++ svn (jamesk's conflicted copy 2015-04-04).sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+a=`cat <<-EOF
+http://zigbee-security.googlecode.com/svn/trunk/ zigbee-security-read-only`
+http://svn.aircrack-ng.org/branch/airoscript-ng/ airoscript-ng
+EOF`
+cd /opt
+for i in $a
+do
+svn checkout $i
+done

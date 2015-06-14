ac524938b17403ff41bfa7854e8055bbc30eecbb
macubergeek <macubergeek@comcast.net>
2015-06-14 07:12:36 -0400

    adding a few tools

--- powershell.sh
+++ powershell.sh
@@ -5,15 +5,10 @@ https://github.com/darkoperator/Posh-SecMod.git
 https://github.com/besimorhino/powercat.git
 https://github.com/samratashok/nishang.git
 https://github.com/vacmf/powershell-scripts.git
-https://github.com/Veil-Framework/Veil-Evasion.git
-https://github.com/Veil-Framework/PowerTools.git
-https://github.com/Veil-Framework/Veil.git
-https://github.com/Veil-Framework/Veil-Pillage.git
-https://github.com/Veil-Framework/Veil-Catapult.git
 https://github.com/rvrsh3ll/POSH-Commander.git
 https://github.com/PyroTek3/PowerShell-AD-Recon.git
 https://github.com/mattifestation/PowerShellArsenal.git
-https://github.com/Veil-Framework/Veil-Pillage.git
+https://github.com/Veil-Framework/Veil.git
 EOF`
 cd /var/www
 for i in $a

b681f4a21a89ea2ec1f11160f1fcb4de30a2a025
macubergeek <macubergeek@comcast.net>
2015-06-24 21:54:19 -0400

    adding a few tools

--- svn.sh
+++ svn.sh
@@ -5,6 +5,7 @@ http://svn.aircrack-ng.org/branch/airoscript-ng/ airoscript-ng
 http://va-pt.googlecode.com/svn/trunk/ va-pt-read-only
 https://svn.code.sf.net/p/jbrofuzz/code/ jbrofuzz
 http://domsnitch.googlecode.com/svn/trunk/ domsnitch-read-only
+http://unix-privesc-check.googlecode.com/svn/trunk/ unix-privesc-check-read-only
 EOF`
 cd /opt
 for i in $a

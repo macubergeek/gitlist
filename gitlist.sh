a16b7ced2697455caef44442ad15a02c05878cee
macubergeek <macubergeek@comcast.net>
2015-09-06 08:51:39 -0400

    adding a few tools

--- gitlist.sh
+++ gitlist.sh
@@ -294,6 +294,7 @@ https://github.com/tiwaryshailesh/MetasploitOnSteroids.git
 https://github.com/mattifestation/WMI_Backdoor.git
 https://github.com/NetSPI/cmdsql.git
 https://github.com/brad-anton/freeradius-wpe.git
+https://github.com/b00stfr3ak/fast_meterpreter.git
 EOF`
 cd ../ 
 for i in $a

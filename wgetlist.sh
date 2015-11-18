4539fee40385e1082e59241b776876f5f8110db1
macubergeek <42jameskelly@gmail.com>
2015-11-18 09:14:20 -0500

    adding a few tools

--- wgetlist.sh
+++ wgetlist.sh
@@ -121,6 +121,7 @@ http://home.btconnect.com/md5decrypter/HashcatGUI_044.zip
 https://github.com/entropy1337/infernal-twin/archive/master.zip
 http://downloads.sourceforge.net/project/isic/isic/ISIC%20v0.07/isic-0.07.tgz
 http://www.thc.org/releases/hydra-8.1.tar.gz
+https://code.google.com/p/littleblackbox/downloads/detail?name=littleblackbox-0.1.3.tar.gz
 EOF`
 cd ../
 for i in $a

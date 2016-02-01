0f3fb0ae24fdc18d9fbc8181e46be39bac658759
macubergeek <42jameskelly@gmail.com>
2016-02-01 14:32:22 -0500

    adding a few tools

--- gitlist.sh
+++ gitlist.sh
@@ -404,6 +404,7 @@ https://github.com/elceef/dnstwist.git
 https://github.com/foxglovesec/Potato.git
 https://github.com/packetfocus/Recon.git
 https://github.com/iamchippa/dumpmon-scraper.git
+https://github.com/Gradberry/Email-Permutator.git
 EOF`
 cd ../ 
 for i in $a
